5b351e1c52cea17e5aa555619275551bbab4fcbc
Wai Loc Leong <111039553+steveleong25@users.noreply.github.com>
2025-09-10 18:51:09 +0800

    Prepare model classes

---
+++ EcommerceWebApp/EcommerceWebApp/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿namespace EcommerceWebApp.Models
+{
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; } = new();
+        public decimal TotalPrice => Items.Sum(i => i.Quantity * i.UnitPrice);
+    }
+
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string UnitOfMeasurement { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ EcommerceWebApp/EcommerceWebApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace EcommerceWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductType { get; set; }
+        public string CatalogVisibility { get; set; }
+        public string StockKeepingUnit { get; set; }
+        public string UnitOfMeasurement { get; set; }
+        public decimal RegularPrice { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
