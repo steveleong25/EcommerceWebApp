b281e493154b1ed98fa911304a0d8fd3e51134e8
Wai Loc Leong <111039553+steveleong25@users.noreply.github.com>
2025-09-13 18:21:27 +0800

    Catalog page

---
+++ EcommerceWebApp/EcommerceWebApp/Controllers/CatalogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWebApp.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public CatalogController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [Route("/Catalog/Category/{category?}")]
+        public IActionResult Category(string category)
+        {
+            var productList = _productService.GetProductsAsync().Result;
+            productList = productList.Where(p => p.Categories.Any(c => c.CategoryName.Equals(category, StringComparison.OrdinalIgnoreCase))).ToList();
+
+            ViewBag.SelectedCategory = category;
+
+            return View(productList);
+        }
+    }
+}
