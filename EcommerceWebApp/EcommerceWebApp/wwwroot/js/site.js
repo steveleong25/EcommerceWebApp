29cd5ac983aae10656a80913b523b013f6a50a21
Wai Loc Leong <111039553+steveleong25@users.noreply.github.com>
2025-09-14 23:02:53 +0800

    update cart badge JS

--- EcommerceWebApp/EcommerceWebApp/wwwroot/js/site.js
+++ EcommerceWebApp/EcommerceWebApp/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+async function updateCartBadge() {
+    try {
+        let response = await fetch('/api/cart/count');
+        let data = await response.json();
+
+        let badge = document.getElementById('cartBadge');
+        if (data.count > 0) {
+            badge.style.display = 'inline-block';
+            badge.textContent = data.count;
+        } else {
+            badge.style.display = 'none';
+        }
+    } catch (e) {
+        console.error("Error fetching cart count", e);
+    }
+}
+
+// update on page load
+updateCartBadge();
