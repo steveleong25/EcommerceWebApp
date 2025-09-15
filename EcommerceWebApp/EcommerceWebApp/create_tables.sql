19bb3e8925d68e93f1f2781a45255c9c4b30aa71
Wai Loc Leong <111039553+steveleong25@users.noreply.github.com>
2025-09-15 23:35:46 +0800

    SQL queries for reference

---
+++ EcommerceWebApp/EcommerceWebApp/create_tables.sql
@@ -0,0 +1,25 @@
+﻿/* MySQL version: 8.4 */
+
+/* Create products table */
+CREATE TABLE `products` (
+  `product_id` int NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_type` varchar(60) NOT NULL,
+  `product_status` varchar(60) NOT NULL,
+  `catalog_visibility` tinyint(1) NOT NULL,
+  `stock_quantity` int NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `image_urls` json DEFAULT NULL,
+  `category` json DEFAULT NULL,
+  `variation_ids` varchar(255) NOT NULL,
+  PRIMARY KEY (`product_id`)
+)
+
+/* Create variations table */
+CREATE TABLE `variations` (
+  `variation_id` int NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `regular_price` decimal(10,2) NOT NULL,
+  `unit_of_measurement` varchar(255) NOT NULL,
+  PRIMARY KEY (`variation_id`)
+) 
