2b930248b3b6a7b9030ab95e87bfa5c58eeba193
Wai Loc Leong <111039553+steveleong25@users.noreply.github.com>
2025-09-15 10:29:43 +0800

    JS search product by name function

--- EcommerceWebApp/EcommerceWebApp/wwwroot/js/site.js
+++ EcommerceWebApp/EcommerceWebApp/wwwroot/js/site.js
@@ -22,3 +22,17 @@ async function updateCartBadge() {
 
 // update on page load
 updateCartBadge();
+
+function filterProducts() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let products = document.querySelectorAll(".product-card");
+
+    products.forEach(product => {
+        let title = product.querySelector(".card-title").textContent.toLowerCase();
+        if (title.includes(input)) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+    });
+}
