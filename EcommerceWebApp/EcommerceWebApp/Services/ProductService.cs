677dc4d8523473b5096881137e42dc0488db9488
Wai Loc Leong <111039553+steveleong25@users.noreply.github.com>
2025-09-16 16:20:32 +0800

    fetch and store products from all pages

--- EcommerceWebApp/EcommerceWebApp/Services/ProductService.cs
+++ EcommerceWebApp/EcommerceWebApp/Services/ProductService.cs
@@ -1,5 +1,6 @@
 ﻿using EcommerceWebApp.Data;
 using EcommerceWebApp.Models;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
@@ -20,20 +21,26 @@ public class ProductService
 
     public async Task<List<Product>> GetProductsAsync()
     {
-        var products = await _productRepository.GetProducts();
-        if (products.Any())
-        {
-            return products.Where(p => p.ProductType.Equals("variable")).ToList();
-        }
-
         var byteArray = Encoding.ASCII.GetBytes($"{_apiSettings.Username}:{_apiSettings.Password}");
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
         _httpClient.DefaultRequestHeaders.Accept.Clear();
         _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+        var productsApiUrl = QueryHelpers.AddQueryString(_apiSettings.GetProductsUrl, "page", "1");
+
         // Call API
-        var response = await _httpClient.GetAsync(_apiSettings.GetProductsUrl);
+        var response = await _httpClient.GetAsync(productsApiUrl);
         response.EnsureSuccessStatusCode();
+
+        response.Headers.TryGetValues("X-WC-Total", out var totalProducts);
+        response.Headers.TryGetValues("X-WC-TotalPages", out var totalPages);
+
+        // Check if database has the same records as API data
+        var products = await _productRepository.GetProducts();
+        if (products.Count == int.Parse(totalProducts.First()))
+        {
+            return products.Where(p => p.ProductType.Equals("variable")).ToList();
+        }
 
         var jsonString = await response.Content.ReadAsStringAsync();
 
@@ -43,6 +50,24 @@ public class ProductService
 
         // Deserialize to the wrapper object first
         var productResponse = JsonConvert.DeserializeObject<ProductResponse>(jsonString);
+
+        var totalNumberOfPages = int.Parse(totalPages.First());
+
+        for (int i = 2; i <= totalNumberOfPages; i++)
+        {
+            productsApiUrl = QueryHelpers.AddQueryString(_apiSettings.GetProductsUrl, "page", (i).ToString());
+
+            response = await _httpClient.GetAsync(productsApiUrl);
+            response.EnsureSuccessStatusCode();
+
+            jsonString = await response.Content.ReadAsStringAsync();
+            jsonData = JsonConvert.DeserializeObject(jsonString);
+
+            _productRepository.AddProducts(jsonData);
+
+            var nextPageProducts = JsonConvert.DeserializeObject<ProductResponse>(jsonString);
+            productResponse.Products.AddRange(nextPageProducts.Products);
+        }
 
         // Return the products list
         return productResponse?.Products ?? new List<Product>();
